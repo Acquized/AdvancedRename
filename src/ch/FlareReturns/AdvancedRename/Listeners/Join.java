d13424cf23947f00cf94ebbf85a418b6f5982cb7
Wyatt J Herkamp <kinglinuxwh@gmail.com>
2016-12-20 09:14:39 -0500

    Update Join.java

--- src/ch/FlareReturns/AdvancedRename/Listeners/Join.java
+++ src/ch/FlareReturns/AdvancedRename/Listeners/Join.java
@@ -13,10 +13,7 @@ public class Join implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
-		if(p.getName().equals("FlareReturns") || p.getName().equals("Beaconplays")) {
-			p.sendMessage(Main.pr + "§7Hey! Dieser Server nutzt dein Plugin §eAdvancedRename§7.");
-			p.playSound(p.getLocation(), Sound.ANVIL_LAND, 10, 10);
-		}
+		
 	}
 
 }
